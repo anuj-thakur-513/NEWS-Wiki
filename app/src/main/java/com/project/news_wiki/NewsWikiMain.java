b9b61f2634b5fa238b6b06609befe25320a9ccf9
anujt <anujthakur2003.anuj@gmail.com>
2022-03-15 08:25:26 +0530

    updated the ad_id permission

--- app/src/main/java/com/project/news_wiki/NewsWikiMain.java
+++ app/src/main/java/com/project/news_wiki/NewsWikiMain.java
@@ -28,10 +28,12 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class NewsWikiMain extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>> {
